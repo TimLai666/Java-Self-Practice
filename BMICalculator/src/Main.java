93ebc9c78753be605b6d3c1d7faf1e3ac2072169
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-07-24 14:13:29 +0800

    Add files via upload

--- BMICalculator/src/Main.java
+++ BMICalculator/src/Main.java
@@ -10,4 +10,18 @@ void main() {
     double bmi = weight / Math.pow(height, 2);
     bmi = Math.round(bmi * 100) / 100.0;
     System.out.println("你的 BMI 是：" + bmi);
+
+    if (bmi < 18.5) {
+        System.out.println("體重過輕");
+    } else if (bmi < 24) {
+        System.out.println("正常範圍");
+    } else if (bmi < 27) {
+        System.out.println("過重");
+    } else if (bmi < 30) {
+        System.out.println("輕度肥胖");
+    } else if (bmi < 35) {
+        System.out.println("中度肥胖");
+    } else {
+        System.out.println("重度肥胖");
+    }
 }
